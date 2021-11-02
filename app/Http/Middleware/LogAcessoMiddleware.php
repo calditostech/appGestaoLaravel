89c63db924e9c658c6d730a3d00488f7004692a4
Ricardo Moreira <calditos1988@outlook.com>
2021-11-01 21:18:40 -0300

    implementação da pagina fornecedores

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\LogAcesso;
+
+
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //$request - manipular
+        //return $next($request);
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
+
+        //return $next($request);
+
+        $resposta = $next($request);
+
+        $resposta->setStatusCode(201, 'O status resposta e o texto da resposta foram modificados||');
+
+        return $resposta;
+
+        //return Response('Chegamos no middleware e finalizamos no proprio middleware');
+    }
+}
--- resources/views/app/layouts/_partials/topo.blade.php
+++ resources/views/app/layouts/_partials/topo.blade.php
@@ -5,7 +5,7 @@
 
     <div class="menu">
         <ul>
-            <li><a href="{{ route('app.home') }}">Home</a></li>
+            <li><a href="{{ route('app.index') }}">Home</a></li>
             <li><a href="{{ route('app.cliente') }}">Cliente</a></li>
             <li><a href="{{ route('app.fornecedor') }}">Fornecedor</a></li>
             <li><a href="{{ route('app.produto') }}">Produto</a></li>
